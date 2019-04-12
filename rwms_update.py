8198493c8bef122b59b2c1410d98fb01400b0d09
shakeyourbunny <shakeyourbunny@gmail.com>
2019-04-12 14:48:25 +0200

    basic version update check skeleton

---
+++ rwms_update.py
@@ -0,0 +1,5 @@
+## RimWorld ModSorter update module
+##
+## checks repo for newly commited versions and (in some point in the future) an inplace upgrade
+
+version_url = "https://gitlab.com/rwms/rwms/raw/master/VERSION"
