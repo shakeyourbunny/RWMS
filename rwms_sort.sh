3c0fb4449565bc44ccc19ec11e59ba36c537aa5d
Jean-Pierre <jeanpierrec19@gmail.com>
2019-05-15 22:55:24 +0200

    updated to check for python version and call correct python instance

--- rwms_sort.sh
+++ rwms_sort.sh
@@ -1,2 +1,8 @@
-#!/usr/bin/env bash
-python rwms_sort.py
+#!/bin/bash
+
+PYVERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:1])))')
+if [ $PYVERSION = 2 ]; then
+    python3 rwms_sort.py
+else
+    python rwms_sort.py
+fi
